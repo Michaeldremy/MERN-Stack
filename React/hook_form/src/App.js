fc8296ac649c84c517944b5c9653489cf22b466b
Michaeldremy <michaeldremy@gmail.com>
2020-05-07 08:47:35 -0700

    more files

---
+++ React/hook_form/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css';
+import UserForm from './components/UserForm';
+
+function App() {
+  const [ state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmpw: "",
+  })
+  return (
+    <div className="App">
+      <UserForm inputs={state} setInputs = {setState}/>
+    </div>
+  );
+}
+
+export default App;
